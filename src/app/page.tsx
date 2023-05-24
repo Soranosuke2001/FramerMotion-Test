7be7989379761b52250f42f81527d449f73958e3
Sora <sorazora1.4@gmail.com>
2023-05-24 16:12:07 -0700

    basic of drag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-around w-full h-screen items-center">
-      <section className="absolute top-2">
-        <h1 className="">Framer Motion Testing</h1>
-      </section>
+    <main className="flex justify-center w-full h-screen items-center">
       <motion.div
         className="border-2 border-black h-36 w-36"
         whileHover={{ scale: 1.7 }}
+        // Allows the user to drag the object horizontally
+        drag="x"
+        // Setting the drag constraints
+        dragConstraints={{ right: 100, left: -200 }}
       ></motion.div>
     </main>
   );
