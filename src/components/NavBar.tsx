7f5c16fd4814d4a787e4bb32675c34a654c31baf
Sora <sorazora1.4@gmail.com>
2023-05-24 16:28:14 -0700

    changed navbar rendering method

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,25 @@
 import Link from "next/link";
 
+const linksList = [
+    {
+        title: 'Home',
+        page: '/'
+    },
+    {
+        title: 'Drag',
+        page: '/drag'
+    },
+]
+
 export default function NavBar() {
   return (
     <nav className="bg-slate-300 h-14">
       <ul className="flex justify-around text-2xl h-full items-center">
-        <li>
-          <Link href="/" >Home</Link>
-        </li>
-        <li>
-          <Link href="/drag">Drag</Link>
-        </li>
+        {linksList.map((link, index) => (
+            <li key={index}>
+                <Link href={`${link.page}`}>{link.title}</Link>
+            </li>
+        ))}
       </ul>
     </nav>
   );
