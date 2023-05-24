54fc0885ed07de20517a89f66bb8d8ebe009f223
Sora <sorazora1.4@gmail.com>
2023-05-24 16:23:25 -0700

    page titles

--- src/app/drag/page.tsx
+++ src/app/drag/page.tsx
@@ -5,6 +5,8 @@ import { motion } from "framer-motion";
 export default function Drag() {
   return (
     <main className="flex justify-center w-full h-screen items-center">
+      <h1 className="absolute top-14 text-3xl">Drag Page</h1>
+
       <motion.div
         className="border-2 border-black h-36 w-36"
         // Allows the user to drag the object horizontally
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 export default function Home() {
   return (
     <main className="flex justify-center w-full h-screen items-center">
+      <h1 className="absolute top-14 text-3xl">Home Page</h1>
       <motion.div
         className="border-2 border-black h-36 w-36"
         // Mouse Hover: scales the box by 1.7 from the original size 
